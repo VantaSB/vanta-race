c7e159e67067f10214e4a56eedf81a3eb7798a2e
AbsoluteXeroEX <mjxstudios@gmail.com>
2019-01-18 22:02:06 -0600

    9

--- stats/effects/secondsight/secondsight.lua
+++ stats/effects/secondsight/secondsight.lua
@@ -1,21 +1,25 @@
 require "/scripts/vec2.lua"
 
 function init()
-  self.enemyDetectRadius = config.getParameter("enemyDetectRadius")
+  self.enemyDetectRadius = 80
   self.enemyDetectQueryParameters = {
     boundMode = "position",
     includedTypes = {"monster","npc"}
   }
 
-  self.enemyDetectTypeNames = {}
+  self.enemyDetectTypeNames = {
+    "crustoise", "iguarmor", "oculob", "pulpin", "snaggler", "tentacrawler", "tintic", "triplod", "apexmutant", "glitchknight", "glitchspider", "po", "pogolem", "sewerfly", "swarpion", "agrobat", "batong", "bobfae", "monopus", "parasprite",
+    "paratail", "pteropod", "scandroid", "tentaclebomb", "tentaclegnat", "tentaclespawner", "erchiusghost", "gosmet", "ignome", "lumoth", "nautileech", "pyromantle", "skimbus", "spookit", "squeem", "tentacleghost", "wisper", "ballista", "helicultist", "ixoling",
+    "kluexsentry", "kluextotem", "minidrone", "moontant", "anglure", "bobot", "bulbop", "capricoat", "crabcano", "crutter", "fennix", "gleap", "hemogoblin", "hemogoblinbutt", "hemogoblinhead", "hypnare", "kingnutmidgeling", "lilodon", "mandraflora", "miasmop",
+    "narfin", "nutmidge", "nutmidgeling", "oogler", "orbide", "peblit", "petricub", "pipkin", "poptop", "punchy", "quagmutt", "rex", "ringram", "scaveran", "smoglin", "snaunt", "snuffish", "sporgus", "tank", "taroni",
+    "tentaclebarrier", "tentacleclam", "toumingo", "trictus", "voltip", "yokat", "fish", "smallfish", "largebiped", "largequadruped", "smallbiped", "smallquadruped"
+  }
 
-  for _, name in ipairs(config.getParameter("enemyDetectTypeNames")) do
+  for _, name in ipairs(self.enemyDetectTypeNames) do
     self.enemyDetectTypeNames[name] = true
   end
 
   self.playerId = entity.id()
-
-  localAnimator.clearDrawables()
 end
 
 function update(dt)
