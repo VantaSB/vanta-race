29d11f0465548b7ae3064553c1bc1c28be37c383
AbsoluteXeroEX <40545575+AbsoluteXeroEX@users.noreply.github.com>
2020-12-06 09:08:36 -0500

    Removed ZB assets

--- zb/zb_scaninteraction.lua
+++
@@ -1,7 +0,0 @@
--- Code derived from the Scan to Learn Blueprint mod by [Cat Emoji] (Literally just the emoji)
-
-function init()
-	message.setHandler("objectScanned", function(_, _, entityName, entityId)
-		world.sendEntityMessage(entityId, 'scanInteraction', player.id())
-	end)
-end
