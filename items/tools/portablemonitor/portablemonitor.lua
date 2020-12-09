2f65728143540810325cd712180fa6bf2a6375fb
AbsoluteXeroEX <40545575+AbsoluteXeroEX@users.noreply.github.com>
2020-12-09 07:45:50 -0500

    Misc item updates

--- items/tools/portablemonitor/portablemonitor.lua
+++ items/tools/portablemonitor/portablemonitor.lua
@@ -4,18 +4,13 @@ function activate(fireMode, shiftHeld)
     activeItem.interact("ScriptPane", "/interface/scripted/corvexquestwindow/questList.config")
   end
 
---Research Tree
-  if fireMode == "primary" and shiftHeld then
-    activeItem.interact("ScriptPane", "/interface/scripted/research/researchTree.config")
-  end
-
 --Mech Assembly
-  if fireMode == "alt" and not shiftHeld then
+  if fireMode == "primary" and shiftHeld then
     activeItem.interact("ScriptPane", "/interface/scripted/mechassembly/mechassemblygui.config")
   end
 
 --Techs Window
-  if fireMode == "alt" and shiftHeld then
+  if fireMode == "alt" and not shiftHeld then
     activeItem.interact("ScriptPane", "/interface/scripted/techupgrade/techupgradegui.config")
   end
 end
