e71d0d382f1daee3df2f204302fe61eb66afd4a3
AbsoluteroEX <mjxstudios@gmail.com>
2021-01-02 16:41:59 -0500

    Dungeon objects updates

--- objects/other/darkgate/darkgate.lua
+++ objects/other/darkgate/darkgate.lua
@@ -2,14 +2,16 @@ require "/scripts/util.lua"
 
 function init()
   self.detectArea = config.getParameter("detectArea")
-  self.detectArea[1] = object.toAbsolutePosition(self.detectArea1[1])
-  self.detectArea[2] = object.toAbsolutePosition(self.detectArea2[2])
+  self.detectArea[1] = object.toAbsolutePosition(self.detectArea[1])
+  self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
+
+  animator.setAnimationState("orb", "on")
 
   storage.active = storage.active or config.getParameter("startActive", false)
 
   message.setHandler("activate", function()
     storage.active = true
-    animator.setAnimationState("gate", "turnon")
+    --animator.setAnimationState("gate", "turnon")
     object.setLightColor(config.getParameter("lightColor", {255, 255, 255}))
   end)
 
@@ -17,43 +19,9 @@ function init()
     return storage.active == true
   end)
 
-  --need to add some custom configuration here, since this isn't technically an ancient gate.
-
-  --[[
-
-  self.activeTime = config.getParameter("activeTime", 60)
-  self.instanceOption = config.getParameter("instanceOption")
-  self.accessConfig = root.assetJson("/interface/scripted/darkgate/gateaccess.config")
-
-  storage.active = storage.active or false
-
-  message.setHandler("activateGate", function()
-    if not storage.active then
-      storage.active = true
-      storage.closeTime = world.time() + self.activeTime
-      math.randomseed(util.seedTime())
-      local instanceOption = util.randomFromList(self.instanceOptions)
-      storage.instanceType = instanceOption[1]
-      storage.instanceWorldId = string.format("InstanceWorld:%s:%s:%s", instanceOption[2], sb.makeUuid(), 8)
-
-      animator.setGlobalTag("destination", storage.instanceType)
-      animator.setAnimationState("portal", "open")
-      animator.playSound("on");
-      object.setLightColor(config.getParameter("lightColor", {255, 255, 255}))
-    end
-  end)
-
-  message.setHandler("closeGate", function()
-    if storage.active then
-      closeGate()
-    end
-  end)
-
   if storage.active then
-    animator.setAnimationState("portal", "off")
-    object.setLightColor({0, 0, 0, 0})
+    animator.setAnimationState("portal", "openloop")
   end
-  ]]
 end
 
 function onInteraction()
@@ -65,5 +33,20 @@ function onInteraction()
 end
 
 function update(dt)
-  --if self.
+  if storage.active then
+    local players = world.entityQuery(self.detectArea[1], self.detectArea[2], {
+      includedTypes = {"player"},
+      boundMode = "CollisionArea"
+    })
+
+    if #players > 0 and animator.animationState("portal") == "closed" then
+      animator.setAnimationState("orb", "off")
+      animator.setAnimationState("portal", "open")
+      object.setLightColor(config.getParameter("lightColor", {255, 255, 255}))
+    elseif #players == 0 and animator.animationState("portal") == "openloop" then
+      animator.setAnimationState("portal", "close")
+      animator.setAnimationState("orb", "on")
+      object.setLightColor({0, 0, 0, 0})
+    end
+  end
 end
