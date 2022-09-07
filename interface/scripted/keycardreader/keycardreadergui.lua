778a3ff32c6a39e13841e1b242f6d245904a5168
AbsoluteXeroEX <boycesa@pm.me>
2022-09-07 12:59:08 -0500

    Interface bits WIP

---
+++ interface/scripted/keycardreader/keycardreadergui.lua
@@ -0,0 +1,41 @@
+function init()
+  self.keycardType = config.getParameter("keycardType", "nightar")
+  if self.keycardType == "nightar" then
+    self.keycardPrefix = "keycard1_"
+    self.labelText = "Nightar Keycard"
+  else
+    self.keycardPrefix = "keycard2_"
+    self.labelText = "Vanta Keycard"
+  end
+  self.requiredLevel = config.getParameter("requiredLevel", 0)
+  if self.requiredLevel == 0 then
+    self.lvText = "Lv.0"
+    self.textColor = {255, 255, 255}
+  elseif self.requiredLevel == 1 then
+    self.lvText = "Lv.1"
+    self.textColor = {0, 127, 255}
+  elseif self.requiredLevel == 2 then
+    self.lvText = "Lv.2"
+    self.textColor = {0, 255, 0}
+  elseif self.requiredLevel == 3 then
+    self.lvText = "Lv.3"
+    self.textColor = {255, 255, 0}
+  elseif self.requiredLevel == 4 then
+    self.lvText = "Lv.4"
+    self.textColor = {255, 0, 0}
+  end
+
+  self.neededKeycard = self.keycardPrefix .. tostring(self.requiredLevel)
+  self.keycardText = self.labelText .. " " .. self.lvText
+end
+
+function update(dt)
+  local current = player.hasItem(self.neededKeycard)
+  widget.setText("itemLabel", self.keycardText)
+  widget.setFontColor("itemLabel", self.textColor)
+  widget.setButtonEnabled("activateButton", current)
+end
+
+function activate()
+  pane.dismiss()
+end
--- interface/scripted/pocsec/pocsecgui.lua
+++ interface/scripted/pocsec/pocsecgui.lua
@@ -1,5 +1,8 @@
 function init()
   local noteText = config.getParameter("noteText", "")
   noteText = noteText:gsub("%^[#%a%x]+;", "")
-  widget.setText("lblNoteText", noteText)
+  local codeImg = config.getParameter("codeImg")
+  widget.setText("lblNoteText.text", noteText)
+  widget.setImage("imgCodeImg", codeImg)
+  sb.logInfo("Loaded code image: ", codeImg)
 end
