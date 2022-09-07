f6aecf77ed4f9cdee2368fdbf68962cfaa854c33
AbsoluteXeroEX <boycesa@pm.me>
2022-09-07 12:58:31 -0500

    Items WIP

--- items/active/unsorted/pocsec/pocsec.lua
+++ items/active/unsorted/pocsec/pocsec.lua
@@ -2,8 +2,7 @@ function activate()
   self.theme = config.getParameter("theme", "default")
   local configData = root.assetJson("/interface/scripted/pocsec/" .. self.theme .. "pocsec.config")
   configData.noteText = config.getParameter("noteText")
-  configData.codeSheet = "/interface/scripted/pocsec/codes/" .. self.theme .. "codes.png"
-  configData.codeImg = config.getParameter("codeImg")
+  configData.codeImg = "/interface/scripted/pocsec/codes/" .. config.getParameter("codeImg", "blank") .. ".png"
   activeItem.interact("ScriptPane", configData)
 
   if config.getParameter("consumeOnUse", true) then
